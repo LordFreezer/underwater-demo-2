64a86953af8eeed9fb325cc08ae7402d3f892257
cmarshall19 <cmarshall19@uco.edu>
2022-02-24 18:39:07 -0600

    moved index.html outside public folder

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./public/index.html"
+            template: "index.html"
         }),
         // new webpack.HotModuleReplacementPlugin()
     ]
